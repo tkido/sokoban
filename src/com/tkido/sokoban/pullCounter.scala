cf2d8b837e8ebb16b090ea6608106d85d9985644
tkido <takanorikido@gmail.com>
2015-09-14 13:46:09 +0900

    Refactoring

--- src/com/tkido/sokoban/pullCounter.scala
+++ src/com/tkido/sokoban/pullCounter.scala
@@ -7,21 +7,18 @@ object PullCounter{
   def apply(data:ProblemData) :Iterable[Array[Int]] = {
     val dec = DeadEndChecker(data)
     def getPullCount(goal:Int): Array[Int] = {
-      val pullcount = Array.fill(data.limit)(Int.MaxValue)
-      def check(v:Int, distance:Int){
-        pullcount(v) = distance
+      def check(v:Int, distance:Int, count:Array[Int]) :Array[Int] = {
+        count(v) = distance
         for (d <- data.neumann)
           if (data.naked(v+d) < WALL &&
               data.naked(v+d*2) < WALL &&
-              distance+1 < pullcount(v+d) &&
+              distance+1 < count(v+d) &&
               !dec.isDeadEnd(v+d*2, v+d) )
-            check(v+d, distance+1)
+            check(v+d, distance+1, count)
+        count
       }
-      check(goal, 0)
-      Log i s"PullCounter pullcount:${pullcount.toList}"
-      pullcount
+      check(goal, 0, Array.fill(data.limit)(Int.MaxValue))
     }
     data.goals.map(getPullCount)
-    
   }
 }
