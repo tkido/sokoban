83eae268f46c216c58100a086705c1979010796c
tkido <takanorikido@gmail.com>
2015-09-17 15:34:55 +0900

    Refactoring Identifier

--- src/com/tkido/sokoban/lockChecker.scala
+++ src/com/tkido/sokoban/lockChecker.scala
@@ -14,25 +14,22 @@ class LockChecker(data:Data) {
   def apply(bags:BitSet, to:Int, direction:Int) :Boolean = {
     val locked = BitSet()
     def sub(v:Int, d:Int, depth:Int) :Boolean = {
-      var rst = false
-      if(locked(v)){
-        rst = true
-      }else if(bags(v)){
-        locked += v
-        rst = (depth > 0 || sub(v+d, d, depth+1)) &&
-              ((!data.canBags(v+r(d)) && !data.canBags(v-r(d))) ||
-               sub(v+r(d),  r(d), depth+1) ||
-               sub(v-r(d), -r(d), depth+1) )
-      }else{
-        rst = !data.canMans(v)
-      }
+      val rst =
+        if(locked(v)){
+          true
+        }else if(bags(v)){
+          locked += v
+          (depth > 0 || sub(v+d, d, depth+1)) &&
+          ((!data.canBags(v+r(d)) && !data.canBags(v-r(d))) ||
+           sub(v+r(d),  r(d), depth+1) ||
+           sub(v-r(d), -r(d), depth+1) )
+        }else{
+          !data.canMans(v)
+        }
       if(!rst) locked -= v
       rst
     }
-    val result = sub(to, direction, 0) &&
-                 !locked.subsetOf(data.goals)
-    if(result) Log w s"Locked status checked!!"
-    result
+    sub(to, direction, 0) && !locked.subsetOf(data.goals)
   }
 }
 
