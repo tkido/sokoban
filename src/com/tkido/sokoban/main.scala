5b83a6b388b86b37453a028901a8bd364a385b35
tkido <takanorikido@gmail.com>
2015-09-25 13:55:56 +0900

    Update Benchmark

--- src/com/tkido/sokoban/main.scala
+++ src/com/tkido/sokoban/main.scala
@@ -1,15 +1,30 @@
 package com.tkido.sokoban
 
-object main extends App {
+object Main extends App {
+  import java.io.File
   import com.tkido.sokoban.Config
   import com.tkido.tools.Log
   import com.tkido.tools.Text
 
   Log.level = Config.logLevel
   
-  val solver = Solver(args.head)
+  def listFiles(filter:File => Boolean)(file:File): List[File] =
+    if (file.isDirectory)
+      file.listFiles.toList.flatMap(listFiles(filter))
+    else
+      List(file).filter(filter)
+  def isValid(file:File) :Boolean =
+    true
+  val root = new File(args.head)
+  val files = listFiles(isValid)(root)
   
-  Log d ((System.currentTimeMillis - executionStart) + "msec")
+  Log d files
+  
+  files.foreach {file =>
+    val now = System.currentTimeMillis
+    Solver(file.toString)
+    Log f s"${System.currentTimeMillis - now}msec"
+  }
   
   Log.close()
 }
